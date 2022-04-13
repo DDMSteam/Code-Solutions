276d9c2a5111568fb55fd9d9668dcc3b5389bda2
Michael Min <michaelmin2020@gmail.com>
2022-04-13 15:27:27 -0500

    Update TwoSum.cs

--- LeetCode/0 TwoSum/TwoSum.cs
+++ LeetCode/0 TwoSum/TwoSum.cs
@@ -1,3 +1,4 @@
+//version 1
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         int[] result = new int[2];
@@ -11,5 +12,20 @@ public class Solution {
             }
         }
         return result;
+    }
+}
+
+//version 2
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length - 1; i++ ) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
     }
 }
