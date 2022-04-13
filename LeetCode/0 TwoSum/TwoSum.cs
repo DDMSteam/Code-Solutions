81c63a19e36a103b44e3b4139b829f2352ebb755
Michael Min <michaelmin2020@gmail.com>
2022-04-13 17:36:22 -0500

    added detailed notes for solutions

--- LeetCode/0 TwoSum/TwoSum.cs
+++ LeetCode/0 TwoSum/TwoSum.cs
@@ -16,16 +16,21 @@ public class Solution {
 }
 
 //version 2
+// [2,11,15,7]
+//  p1,p2   <-- initialize position in the array
+
 public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.Length - 1; i++ ) {
-            for (int j = i + 1; j < nums.Length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
+    public int[] TwoSum(int[] nums, int target) {             
+        for (int p1 = 0; p1 < nums.Length; p1++ ) { //initialize pointer position at the 0th indices of the array and iterate through the entire length of the array
+            for (int p2 = p1 + 1; p2 < nums.Length; p2++) { //initialize pointer position at the 1st indices of the array (the index right next too p1) and iterate through the entire length of the array. This for loop will iterate through the entire are first then, the for loop before it will iterate, then the 2nd nested for will iterate through the remaining indices.
+                if (nums[p1] + nums[p2] == target) { //2 + 7 = 9 compare the values of the both pointers current position in the for loop and check if the both add up to the target value
+                    return new int[] {p1, p2}; //create a new array storing int values and return the current indices of p1 and p2 
                 }
             }
         }
-        // In case there is no solution, we'll just return null
+        // In case there is no solution, we'll just return null (edge case to factor in if there is no solution)
         return null;
     }
 }
+
+//Time complexity is O(n²) because of two nested for loops. 
---
+++ LeetCode/0 TwoSum/twoSum.py
@@ -0,0 +1,27 @@
+
+
+#version 1
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[j] == target - nums[i]:
+#                     return [i, j]
+
+#version 2
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for p1 in range(len(nums)): #initialize pointer position at the 0th indices of the array and iterate through the entire length of the array
+            for p2 in range(p1 + 1, len(nums)): #initialize pointer position at the 1st indices of the array (the index right next too p1) and iterate through the entire length of the array
+                if  nums[p1] + nums[p2] == target: #2 + 7 = 9 compare the values of the both pointers current position in the for loop and check if the both add up to the target value
+                    return [p1, p2] #return the current indices of p1 and p2 
+
+# [ 2, 7, 11, 1]
+#  p1,p2   <-- initialize position in the array
+
+#nums = [2,7,11,15]
+nums = [2,11,15,7]
+target = 9
+s = Solution()
+answer = s.twoSum(nums, target)
+print(answer)
